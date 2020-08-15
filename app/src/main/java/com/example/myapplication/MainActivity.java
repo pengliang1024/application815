b5ee0211cecfad2be1877c9808c0492e11ca94fa
pengliang <15616205476@163.com>
2020-08-15 14:52:57 +0800

    iiii

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // xiaomuxa11 ======
         Toast.makeText(this, "hahaha", Toast.LENGTH_SHORT).show();
 
     }
