8d2b40b53af819e6edf43454c41e10b1d28df7c0
pengliang <15616205476@163.com>
2020-08-15 14:49:52 +0800

    iiii

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "hahaha", Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this, "download", Toast.LENGTH_SHORT).show();
     }
 }
